d86352548356e92cd011a83cd813f51571ffb04b
Maxwell A. Borden <maxwellborden@gmail.com>
2018-10-25 17:29:49 -0700

    creating a better structure, taking advantage of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const uglify = require('uglifyjs-webpack-plugin')
 const path = require('path')
 
-module.exports = {
+module.exports = env => ({
   entry: {
-    content: path.join(__dirname, 'src', 'main.js'),
-    popup: path.join(__dirname, 'src', 'popup.js'),
+    content: path.join(__dirname, 'src', 'content', 'main.js'),
+    popup: path.join(__dirname, 'src', 'popup', 'main.js'),
   },
   node: {fs: 'empty'},
   output: {
@@ -13,6 +14,18 @@ module.exports = {
     filename: '[name]-bundle.js'
   },
   plugins: [
+    new CopyWebpackPlugin([{
+      from: './**',
+      context: 'src/static/',
+      transform(content, path) {
+        if (path.indexOf('manifest.json') >= 0) {
+          let manifest = JSON.parse(content.toString());
+          manifest.version = env.version
+          return JSON.stringify(manifest);
+        }
+        return content;
+      }
+    }]),
     new uglify({ uglifyOptions: { warnings: false } }),
   ]
-};
+});
